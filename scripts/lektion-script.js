2318849ea5b941b24bb7392ead0241fc2a4fbb60
Albert <dony.nyc@gmail.com>
2024-02-04 16:26:46 +0100

    lek: toggler improved

--- scripts/lektion-script.js
+++ scripts/lektion-script.js
@@ -1,35 +1,5 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     const descriptionElement = document.querySelector('.lek-description');
-//     const contentElement = document.querySelector('.lek-description__content');
-//     const arrowToggler = document.querySelector('.arrow-toggler');
-
-//     console.log(contentElement, descriptionElement);
-
-//     descriptionElement.addEventListener('click', toggleDisplay);
-
-//     // function toggleDisplay() {
-//     //     if (contentElement.style.display === 'none' || contentElement.style.display === '') {
-//     //         contentElement.style.display = 'flex';
-//     //         descriptionElement.style.cursor = 'default';
-//     //     } else {
-//     //         contentElement.style.display = 'none';
-//     //     }
-//     // }
-
-//     function toggleDisplay() {
-//         if (contentElement.style.transform === 'translateY(0%)') {
-//             contentElement.style.transform = 'translateY(-100%)';
-//             arrowToggler.classList.remove('rotated');
-//         } else {
-//             contentElement.style.transform = 'translateY(0%)';
-//             arrowToggler.classList.add('rotated');
-//         }
-//     }
-
-// });
 
 document.addEventListener('DOMContentLoaded', function () {
     const descriptionElement = document.querySelector('.lek-description');
@@ -38,18 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Функция для изменения свойства display и поворота стрелки
     function toggleDisplay() {
-        if (contentElement.style.maxHeight === '0px') {
+        if (contentElement.style.maxHeight === '0px'
+            || contentElement.style.maxHeight === '') {
             contentElement.style.maxHeight = '100%';
             contentElement.style.marginTop = '50px'; // Задайте здесь высоту, которую вы хотите показать
             arrowToggler.classList.add('rotated');
+            descriptionElement.style.cursor = 'default';
         } else {
-            contentElement.style.maxHeight = '0px';
-            contentElement.style.marginTop = '0px';
-            arrowToggler.classList.remove('rotated');
+            if (event.target.classList.contains('arrow-toggler')) {
+                contentElement.style.maxHeight = '';
+                contentElement.style.marginTop = '';
+                arrowToggler.classList.remove('rotated');
+                descriptionElement.style.cursor = '';
+            }
         }
     }
 
-    // Добавляем обработчик события на клик
     descriptionElement.addEventListener('click', toggleDisplay);
 });
 
