449fa3463232211b747839b28ea5e6a225bc0eb6
Albert <dony.nyc@gmail.com>
2024-02-08 23:58:13 +0100

    list: bottom slider

--- scripts/list-script.js
+++ scripts/list-script.js
@@ -1,31 +1,53 @@
 // function slider({ container, slide, prevArrow, nextArrow,
 //     totalCounter, currentCounter, wrapper, field }) {
 window.addEventListener('DOMContentLoaded', () => {
+
+    let offset = 0;
+
     const slides = document.
         querySelector('.categories__list-list').querySelectorAll('.categories__list-item'),
         prevBtn = document.querySelector('.left'),
         nextBtn = document.querySelector('.right'),
         slidesWrapper = document.querySelector('.slides-wrapper'),
         slidesField = document.querySelector('.categories__list-list'),
-        width = window.getComputedStyle(slidesWrapper).width;
-
-    console.log(prevBtn, nextBtn);
+        width = window.getComputedStyle(slides[0]).width;
 
     slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
     slidesField.style.transition = '0.5s all';
     slidesWrapper.style.overflow = 'hidden';
+    slides.forEach((slide) => { slide.style.marginRight = `20px` });
+
+    // function strToDigits(str) {
+    //     return +str.replace(/\D/g, '');
+    // }
 
     function strToDigits(str) {
-        return +str.replace(/\D/g, '');
+        const match = str.match(/(\d+\.\d+|\d+)/);
+        return match ? +match[0] + 20 : NaN;
     }
 
+    console.log(strToDigits(width) / 3, width);
+
     nextBtn.addEventListener('click', () => {
-        slidesField.style.transform = `translateX(${strToDigits(width) / 3}px)`;
+        if (offset ==
+            strToDigits(width) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += strToDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
     });
 
     prevBtn.addEventListener('click', () => {
-        slidesField.style.transform = `translateX(-${strToDigits(width) / 3}px)`;
+        if (offset == 0) {
+            offset = strToDigits(width) * (slides.length - 1);
+        } else {
+            offset -= strToDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
     });
 
 });
